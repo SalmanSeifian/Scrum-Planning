631537a686ffbcd354e1dda18e1543d64ed2350f
Unknown <salman.seifian@gmail.com>
2019-07-03 01:46:13 +0430

    Revert "Revert "Initial commit""
    This reverts commit 97056722be9ccfadf80fb2a1504973483150cb89.

---
+++ app/src/main/java/com/scrumplanning/create/CreateRoomViewModel.kt
@@ -0,0 +1,16 @@
+package com.scrumplanning.create
+
+import androidx.lifecycle.ViewModel
+
+class CreateRoomViewModel(): ViewModel(){
+
+
+    fun onRoomCreate(){
+
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
---
+++ app/src/main/java/com/scrumplanning/create/CreateRoomViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.scrumplanning.create
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class CreateRoomViewModelFactory : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CreateRoomViewModel::class.java)) {
+            return CreateRoomViewModel() as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
