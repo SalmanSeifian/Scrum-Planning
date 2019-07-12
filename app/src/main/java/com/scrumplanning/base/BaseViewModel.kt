22100b2625c51cf1638d40e253d26b77e5edabfd
Unknown <salman.seifian@gmail.com>
2019-07-12 12:52:38 +0430

    [*] inject required dependencies to abstract base view model

--- app/src/main/java/com/scrumplanning/base/BaseViewModel.kt
+++ app/src/main/java/com/scrumplanning/base/BaseViewModel.kt
@@ -1,11 +1,33 @@
 package com.scrumplanning.base
 
 import androidx.lifecycle.ViewModel
+import com.scrumplanning.data.remote.ApiInterface
+import com.scrumplanning.di.component.DaggerViewModelComponent
+import com.scrumplanning.di.component.ViewModelComponent
+import com.scrumplanning.di.module.NetworkModule
+import com.scrumplanning.ui.create.CreateRoomViewModel
+import com.scrumplanning.utils.BASE_URL
 import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
 
 abstract class BaseViewModel : ViewModel() {
 
-    private lateinit var compositeDisposable: CompositeDisposable
+    var compositeDisposable: CompositeDisposable = CompositeDisposable()
+    private val viewModelComponent: ViewModelComponent = DaggerViewModelComponent
+        .builder()
+        .networkModule(NetworkModule(BASE_URL))
+        .build()
+
+    init {
+        inject()
+    }
+
+    private fun inject() {
+
+        when (this) {
+            is CreateRoomViewModel -> viewModelComponent.inject(this)
+        }
+    }
 
 
     override fun onCleared() {
